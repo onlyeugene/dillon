6bea437d9ddb9cfd4d452493bc639cb35f3f592c
onlyeugene <eugeneokogun@gmail.com>
2025-05-29 15:26:11 +0100

    changed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/index";
-import Home from "./pages/Home";
+import Home from "@/pages/home/index";
 
 
 function App() {
